641bee3031abfc463a829325a8dc1d2fb3aa483b
Zbolt50 <zekeboggs10@gmail.com>
2025-01-16 22:34:28 -0500

    changed keymap

--- config.h
+++ config.h
@@ -4,24 +4,21 @@
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP8
 #define SERIAL_USART_RX_PIN GP9
-//#define SERIAL_PIO_USE_PIO1  //Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
+// #define SERIAL_PIO_USE_PIO1  //Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
 
 #define I2C_DRIVER I2CD0
 #define I2C1_SCL_PIN GP13
 #define I2C1_SDA_PIN GP12
 
-
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_OLED_ENABLE
 #define SPLIT_LED_STATE_ENABLE
-
 #define EE_HANDS
-//#define MASTER_LEFT
+// #define MASTER_LEFT
 
-#define BOOTMAGIC_LITE_ROW_RIGHT 0    
+#define BOOTMAGIC_LITE_ROW_RIGHT 0
 #define BOOTMAGIC_LITE_COLUMN_RIGHT 5
 
 // Tap Dance Config
-#define TAPPING_TERM 175
+#define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
-
--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -1,3 +1,4 @@
+
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -29,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC,      KC_1,          KC_2,         KC_3,         KC_4,        KC_5,                KC_6,     KC_7,           KC_8,         KC_9,         KC_0,           KC_DEL,
         KC_TAB,       KC_Q,          KC_W,         KC_E,         KC_R,        KC_T,                KC_Y,     KC_U,           KC_I,         KC_O,         KC_P,           KC_BSLS,
         KC_ESC,       LSFT_T(KC_A),  LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F),KC_G,                KC_H,     RGUI_T(KC_J),   RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN),KC_ENT,
-        MO(_SYMBOLS), KC_Z,          KC_X,         KC_C,         KC_V,        KC_B,                KC_N,     KC_M,           KC_COMMA,     KC_DOT,       KC_SLSH,        KC_QUOTE,
+        MO(_SYMBOLS), KC_Z,          KC_X,         KC_C,         KC_V,        KC_B,                KC_N,     KC_M,           KC_COMMA,     KC_DOT,       KC_SLSH,        MO(_FUNC),
                                                                  LT(_SYMBOLS, KC_BSPC),KC_SPACE,   KC_SPACE, LT(_FUNC, KC_BSPC)
 
 
@@ -37,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYMBOLS] = LAYOUT(
 
         QK_GESC,     KC_1,          KC_2,         KC_3,         KC_4,        KC_5,                  KC_6,     KC_7,           KC_8,         KC_9,         KC_0,           _______,
-        KC_TAB,      _______,       _______,      _______,      _______,     _______,               _______,  KC_MINS,        KC_EQL,       KC_TILDE,     KC_GRAVE,       _______,
-        KC_ESC,      _______,       _______,      _______,      _______,     _______,               KC_H,     TD(TD_PAREN),   TD(TD_BRACK), TD(TD_BRACE), _______,        _______,
+        KC_TAB,      KC_Q,          KC_W,         KC_E,         KC_R,        KC_T,                  KC_Y,     KC_MINS,        KC_EQL,       KC_O,         KC_GRAVE,       KC_GRAVE,
+        KC_ESC,      _______,       _______,      _______,      _______,     _______,               KC_H,     TD(TD_PAREN),   TD(TD_BRACK), TD(TD_BRACE), KC_QUOTE,       KC_ENT,
         _______,     _______,       _______,      _______,      _______,     _______,               KC_N,     KC_M,           KC_COMMA,     KC_DOT,       KC_SLSH,        _______,
                                                                 _______,     _______,               _______,  _______
     ),
@@ -54,6 +55,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 // clang-format on
+#ifdef RGBLIGHT_ENABLE
+switch (get_highest_layer(layer_state)) {
+    case _QWERTY:
+        rgblight_setrgb_at(255, 0, 0, 0);
+        break;
+
+    case _FUNC:
+        rgblight_setrgb_at(0, 255, 0, 0);
+        break;
+
+    case _SYMBOLS:
+        rgblight_setrgb_at(0, 0, 255, 0);
+        break;
+}
+#endif /* ifdef RGBLIGHT_ENABLE */
 #ifdef OLED_ENABLE
 
 static void render_logo(void) {
