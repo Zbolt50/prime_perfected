0b8e2037438f83b8238777f889c9f85add0ce0ae
Zbolt50 <zekeboggs10@gmail.com>
2025-02-15 23:31:45 -0500

    added new glitch animation and some more layers

--- config.h
+++ config.h
@@ -4,7 +4,6 @@
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP8
 #define SERIAL_USART_RX_PIN GP9
-// #define SERIAL_PIO_USE_PIO1  //Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
 
 #define I2C_DRIVER I2CD0
 #define I2C1_SCL_PIN GP13
@@ -16,9 +15,11 @@
 #define EE_HANDS
 // #define MASTER_LEFT
 
-#define BOOTMAGIC_LITE_ROW_RIGHT 0
-#define BOOTMAGIC_LITE_COLUMN_RIGHT 5
+#define BOOTMAGIC_LITE_ROW_RIGHT 2
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 6
 
+#define BOOTMAGIC_LITE_ROW_LEFT 3
+#define BOOTMAGIC_LITE_COLUMN_LEFT 3
 // Tap Dance Config
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
 #define TAPPING_TERM_PER_KEY
